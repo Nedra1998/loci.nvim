c6b8092be65b5f1fee4efb2354c97ea9d8b37add
Arden Rasmussen <ardenisthebest@gmail.com>
2021-04-24 13:03:56 -0700

    Restricted workspace setup files

--- lua/loci/init.lua
+++ lua/loci/init.lua
@@ -10,8 +10,9 @@ function M.setup(opts)
     config = tbl.apply_defaults(opts, config)
     cmd('augroup LociSetupHook')
     cmd('autocmd! *')
-    cmd('autocmd BufEnter ' .. config.directory ..
-            '* lua require("loci").setup_hook()')
+    cmd('autocmd BufEnter ' .. config.directory .. '/**/*.(' ..
+            table.concat(config.extensions, '|') ..
+            ') lua require("loci").setup_hook()')
     cmd('augroup END')
 end
 
